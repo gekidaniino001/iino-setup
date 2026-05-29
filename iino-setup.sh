a6829fa8a8837a7135fa34870131d1edd0c17766
Masao KONDOH <kondoh@axe.bz>
2026-05-29 12:59:24 +0900

    fix(iino-setup.sh): add for test

--- iino-setup.sh
+++ iino-setup.sh
@@ -182,4 +182,9 @@ sudo chmod 600 ~/.ssh/id_rsa
 cd ~
 git clone git@github.com:gekidaniino001/iinomob2.autoware
 cd ~/iinomob2.autoware
+
+# for test
+git fetch origin kon_fix_ssd_lan
+git switch kon_fix_ssd_lan
+
 bash install.sh
