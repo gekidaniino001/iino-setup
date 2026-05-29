0a6664eea0ff0dfc8e9d34a7ef6fb21c4fa97e24
Masao KONDOH <kondoh@axe.bz>
2026-05-29 14:28:06 +0900

    fix(iino-setup.sh): add rosdistro

--- iino-setup.sh
+++ iino-setup.sh
@@ -62,6 +62,7 @@ rosdep update
 ## wget -O /tmp/amd64.env https://raw.githubusercontent.com/autowarefoundation/autoware/main/amd64.env && source /tmp/amd64.env
 ## URL not found 2026/05
 rmw_implementation=rmw_cyclonedds_cpp
+rosdistro=$ROS_DISTRO
 
 # For details: https://docs.ros.org/en/humble/How-To-Guides/Working-with-multiple-RMW-implementations.html
 rmw_implementation_dashed=$(eval sed -e "s/_/-/g" <<< "${rmw_implementation}")
